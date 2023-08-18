0c025b0a702665734c35f89253fdfab970db1b49
annanya <annanyasmathur2017@gmail.com>
2023-08-18 21:26:18 +0530

    About

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -35,5 +35,52 @@ export class DashboardComponent implements OnInit
      this.ProjectCost=2113507;
      this.CurrentExpenditure=96788;
      this.AvailableFunds=52536;
- }
+
+     this.Clients=["ABC Infotech LTD" ,"DEF Software Solutions", "GHI Industries"];
+     this.Projects=["Project A","Project B","Project C","Project D","Project E","Project F"];
+     for (var i=2023; i>=2015;i--)
+     {
+      this.Years.push(i);
+     }
+     this.TeamMembersSummary=[
+      {Region:"East",TeamMembersCount:20, TemporarilyUnavailableMembers:4},
+      {Region:"West",TeamMembersCount:15, TemporarilyUnavailableMembers:8},
+      {Region:"South",TeamMembersCount:17, TemporarilyUnavailableMembers:1},
+      {Region:"North",TeamMembersCount:15, TemporarilyUnavailableMembers:6}
+     ];
+     this.TeamMembers=[
+      { Region:"East" ,Members :[
+        {ID:1, Name:"Ford",Status:"Available"},
+        {ID:2, Name:"Miller",Status:"Available"},
+        {ID:3, Name:"Jones",Status:"Busy"},
+        {ID:4, Name:"James",Status:"Busy"}
+
+      ]},
+
+      { Region:"West" ,Members :[
+        {ID:1, Name:"Ford",Status:"Available"},
+        {ID:2, Name:"Miller",Status:"Available"},
+        {ID:3, Name:"Jones",Status:"Busy"},
+        {ID:4, Name:"James",Status:"Busy"}
+
+      ]},
+
+      { Region:"South" ,Members :[
+        {ID:1, Name:"Ford",Status:"Available"},
+        {ID:2, Name:"Miller",Status:"Available"},
+        {ID:3, Name:"Jones",Status:"Busy"},
+        {ID:4, Name:"James",Status:"Busy"}
+
+      ]},
+
+      { Region:"North" ,Members :[
+        {ID:1, Name:"Ford",Status:"Available"},
+        {ID:2, Name:"Miller",Status:"Available"},
+        {ID:3, Name:"Jones",Status:"Busy"},
+        {ID:4, Name:"James",Status:"Busy"}
+
+      ]},
+
+     ];
+    }
 }
