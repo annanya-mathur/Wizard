d4527dfbeefee602d11600775d8c67bce9b2e0cb
annanya <annanyasmathur2017@gmail.com>
2023-08-19 19:11:38 +0530

    Initial Commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -83,4 +83,80 @@ export class DashboardComponent implements OnInit
 
      ];
     }
+    onProjectChange($event:any)
+    {
+      if($event.target.innerHTML == "Project A")
+      {
+        this.ProjectCost=2113507;
+        this.CurrentExpenditure=96788;
+        this.AvailableFunds=52536;
+
+   
+      }
+      else if($event.target.innerHTML == "Project B")
+      {
+        this.ProjectCost=98735073;
+        this.CurrentExpenditure=80967;
+        this.AvailableFunds=41525;
+        this.TeamMembersSummary=[
+          {Region:"East",TeamMembersCount:25, TemporarilyUnavailableMembers:14},
+          {Region:"West",TeamMembersCount:15, TemporarilyUnavailableMembers:5},
+          {Region:"South",TeamMembersCount:12, TemporarilyUnavailableMembers:1},
+          {Region:"North",TeamMembersCount:10, TemporarilyUnavailableMembers:6}
+         ];
+   
+      }
+      else if($event.target.innerHTML == "Project C")
+      {
+        this.ProjectCost=98675365;
+        this.CurrentExpenditure=30967;
+        this.AvailableFunds=38525;
+        this.TeamMembersSummary=[
+          {Region:"East",TeamMembersCount:20, TemporarilyUnavailableMembers:4},
+          {Region:"West",TeamMembersCount:15, TemporarilyUnavailableMembers:8},
+          {Region:"South",TeamMembersCount:17, TemporarilyUnavailableMembers:1},
+          {Region:"North",TeamMembersCount:15, TemporarilyUnavailableMembers:6}
+         ];
+   
+      }
+      else if($event.target.innerHTML == "Project D")
+      {
+        this.ProjectCost=23890435;
+        this.CurrentExpenditure=80967;
+        this.AvailableFunds=51536;
+        this.TeamMembersSummary=[
+          {Region:"East",TeamMembersCount:20, TemporarilyUnavailableMembers:4},
+          {Region:"West",TeamMembersCount:15, TemporarilyUnavailableMembers:8},
+          {Region:"South",TeamMembersCount:17, TemporarilyUnavailableMembers:1},
+          {Region:"North",TeamMembersCount:15, TemporarilyUnavailableMembers:6}
+         ];
+   
+      }
+      else if($event.target.innerHTML == "Project E")
+      {
+        this.ProjectCost=986321690;
+        this.CurrentExpenditure=42967;
+        this.AvailableFunds=57525;
+        this.TeamMembersSummary=[
+          {Region:"East",TeamMembersCount:20, TemporarilyUnavailableMembers:4},
+          {Region:"West",TeamMembersCount:15, TemporarilyUnavailableMembers:8},
+          {Region:"South",TeamMembersCount:17, TemporarilyUnavailableMembers:1},
+          {Region:"North",TeamMembersCount:15, TemporarilyUnavailableMembers:6}
+         ];
+   
+      }
+      else if($event.target.innerHTML == "Project F")
+      {
+        this.ProjectCost=67432965;
+        this.CurrentExpenditure=78967;
+        this.AvailableFunds=34525;
+        this.TeamMembersSummary=[
+          {Region:"East",TeamMembersCount:20, TemporarilyUnavailableMembers:4},
+          {Region:"West",TeamMembersCount:15, TemporarilyUnavailableMembers:8},
+          {Region:"South",TeamMembersCount:17, TemporarilyUnavailableMembers:1},
+          {Region:"North",TeamMembersCount:15, TemporarilyUnavailableMembers:6}
+         ];
+   
+      }
+    }
 }
